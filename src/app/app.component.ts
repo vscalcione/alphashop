6094074294392e5bc7c35fb53a8f31b3494c76ea
vscalcione <vincenzo99.scalcione@gmail.com>
2020-02-02 16:07:03 +0100

    Revert "initial commit"
    This reverts commit 3f86d2f3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'alphashop';
-  regards = 'Welcome in alphashop web application';
-  subtitle = 'Select your favourit article to shop it';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { WelcomeComponent } from './welcome/welcome.component';
-import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    WelcomeComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
